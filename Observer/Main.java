a37a2f6176a7e028e2752494bb268073acd97602
wang_lea <wang1345@e.ntu.edu.sg>
2023-07-02 11:11:57 +0800

    observer pattern with pull style of communication

---
+++ Observer/Main.java
@@ -0,0 +1,18 @@
+package Observer;
+
+import javax.xml.crypto.Data;
+
+public class Main {
+    public static void main(String[] args) {
+        var datasource  = new DataSource();
+        var sheet1 = new SpreadSheet(datasource);
+        var sheet2 = new SpreadSheet(datasource);
+        var chart = new Chart(datasource);
+
+        datasource.addObserver(sheet1);
+        datasource.addObserver(sheet2);
+        datasource.addObserver(chart);
+
+        datasource.setValue(1);
+    }
+}
